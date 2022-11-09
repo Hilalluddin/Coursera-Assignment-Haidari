746cb1d20a746b2c3240f2ed097ad15230fe2236
Hilalluddin <115922347+Hilalluddin@users.noreply.github.com>
2022-11-09 19:45:52 +0100

    Create scribt.js

---
+++ scribt.js
@@ -0,0 +1,10 @@
+var names = ["Jens", "John"];
+
+for (var i = 0; i < names.length; i++) {
+	console.log("Good bye: " + names[i]);
+}
+
+names = ["Bob", "Christian", "Rock"];
+for (var i = 0; i < names.length; i++) {
+	console.log("Hello: " + names[i]);
+}
